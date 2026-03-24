72762a64f8a9908d782045d936010bc3c7cab572
Axel1208 <juandavidospinagutierrez44gmail.com>
2026-03-24 18:13:52 -0500

    Validaciones para los roles Instrucotr,Aprendiz y Administrador usando todos los middlewares

--- backend/src/middlewares/verificarRol.js
+++ backend/src/middlewares/verificarRol.js
@@ -1,14 +1,11 @@
 module.exports = function (...rolesPermitidos) {
   return (req, res, next) => {
-
-    // 🔐 Validar autenticación previa
     if (!req.usuario) {
       return res.status(401).json({
         mensaje: "No autenticado"
       });
     }
 
-    // 🔐 Validar rol
     if (!rolesPermitidos.includes(req.usuario.rol)) {
       return res.status(403).json({
         mensaje: "Acceso denegado"

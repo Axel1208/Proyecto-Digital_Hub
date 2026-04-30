01bf12a454e8794fdc8cbadc841c6c81a65fb7bf
Axel1208 <juandavidospinagutierrez44gmail.com>
2026-04-30 11:35:41 -0500

    cambios

--- backend/src/app.js
+++ backend/src/app.js
@@ -71,6 +71,6 @@ app.use((req, res) => {
 // ===============================
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
     console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
 });
